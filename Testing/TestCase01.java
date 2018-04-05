eae16deb573c55a89284d6ece7b61c228ff19965
Tyler S. Townsend <ttown@knights.ucf.edu>
2018-04-05 00:54:45 -0400

    Update TestCase01.java

--- Testing/TestCase01.java
+++ Testing/TestCase01.java
@@ -5,8 +5,7 @@ public class TestCase1 {
 
 	public static void main(String [] args) throws Exception {
 		Scanner sc = new Scanner(new File("data/sample_input/test1.txt"));
-		// Scanner sc = new Scanner(new File("data/sample_input/test1.in"));
-		Graph g1 = new Graph();
+		Graph g1 = Graph.makeGraph();
 		int numNodes = sc.nextInt();
 		int cases;
 		for (int i=1; i<=numNodes; i++) {
@@ -18,12 +17,5 @@ public class TestCase1 {
 		System.out.println("Graph g1");
 		g1.printGraph();
 		System.out.println();
-		System.out.println("Tree t1");
-		Tree t1 = Algorithm.bfs(g1, 1);
-		t1.printGraph();
-		System.out.println();
-		System.out.println("Tree t2");
-		Tree t2 = Algorithm.dfs(g1, 1);
-		t2.printGraph();
 	}
 }
